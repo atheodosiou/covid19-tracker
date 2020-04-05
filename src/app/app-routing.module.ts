179a35465dd6557b2c63db9f7cc7c3223124a01d
tasos <theodosiout@or-co.gr>
2020-04-05 21:06:05 +0300

    Adds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',pathMatch:'full',redirectTo:'/home'},
+  {path:'home',loadChildren:()=>import('./shared/components/home/home.module').then(m=>m.HomeModule)},
+  {path:'**',redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
