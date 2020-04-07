65296e28e1a8fbedc0ed13bb6d3015c003b8da29
tasos <theodosiout@or-co.gr>
2020-04-07 13:27:59 +0300

    Update map scruture

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -20,6 +20,25 @@ export class MapComponent implements OnInit {
   map: L.Map;
   private mapData: any[];
   center: L.LatLng = new L.LatLng(37.2532002, 5.8725402);
+  Stadia_AlidadeSmoothDark = L.tileLayer(
+    "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+    {
+      maxZoom: 20,
+      attribution:
+        '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    }
+  );
+
+  OpenStreetMap_DE = L.tileLayer(
+    "https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png",
+    {
+      maxZoom: 18,
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        
+    }
+  );
+
   ngOnInit() {
     setTimeout(() => {
       this.initMap().then(() => {
@@ -31,24 +50,7 @@ export class MapComponent implements OnInit {
   }
 
   private initMap(): Promise<any> {
-    const Stadia_AlidadeSmoothDark = L.tileLayer(
-      "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-      {
-        maxZoom: 20,
-        attribution:
-          '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-      }
-    );
-
-    const OpenStreetMap_DE = L.tileLayer(
-      "https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png",
-      {
-        maxZoom: 18,
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-          
-      }
-    );
+    
 
     // const totalConfirmed=this.mapData.map(x=>x+=x.stats.confirmed);
     // console.log('TOTAL CASES:',totalConfirmed);
@@ -57,29 +59,43 @@ export class MapComponent implements OnInit {
     return [point.coordinates.latitude,point.coordinates.longitude,0.5];
     });
     console.log('HEAT DATA',heatData);
-    const heatLayer=L.heatLayer(heatData as L.HeatLatLngTuple[],{blur:15,gradient:{0.4: 'blue', 0.65: 'lime', 1: 'red'}});
+    // const heatLayer=L.heatLayer(heatData as L.HeatLatLngTuple[],{blur:15,gradient:{0.4: 'blue', 0.65: 'lime', 1: 'red'}});
     const layers = {
-      Light: OpenStreetMap_DE,
-      Dark: Stadia_AlidadeSmoothDark,
-      Heatmap:heatLayer
+      Light: this.OpenStreetMap_DE,
+      Dark: this.Stadia_AlidadeSmoothDark,
+      // Heatmap:heatLayer
     };
+
+
     return new Promise<any>((fullfiled, rejected) => {
+      // Markers
+      const markers=[];
+      this.mapData.forEach((item) => {
+        const marker = L.marker([item.coordinates.latitude, item.coordinates.longitude], {
+          icon: this.getIcon(item.stats.confirmed),
+        }).bindPopup(`<strong>Confirmed: </strong>${item.stats.confirmed}<br><strong>Deaths: </strong>${item.stats.deaths}<br><strong>Recovered: </strong>${item.stats.recovered}`)
+        markers.push(marker);
+      });
+      const markerLayerGroup = L.layerGroup([...markers]);
+
       this.map = L.map("map", {
         center: this.center,
         zoom: 3,
-        layers: [Stadia_AlidadeSmoothDark, OpenStreetMap_DE,heatLayer],
-
+        layers: [this.Stadia_AlidadeSmoothDark, markerLayerGroup]
       });
-      L.control.layers(layers).addTo(this.map);
+
+      const baseMaps = {
+        Light:this.OpenStreetMap_DE,
+        Grayscale:this.Stadia_AlidadeSmoothDark
+      };
+      const overlayMaps={
+        Countries: markerLayerGroup
+      };
+
+      // Add Controls
+      L.control.layers(baseMaps,overlayMaps).addTo(this.map);
       L.control.scale().addTo(this.map);
-      
-      //Markers
-      // this.mapData.forEach((item) => {
-      //   L.marker([item.coordinates.latitude, item.coordinates.longitude], {
-      //     icon: this.getIcon(item.stats.confirmed),
-      //   }).addTo(this.map);
-      // });
-      
+
       fullfiled();
     });
   }
@@ -123,37 +139,37 @@ export class MapComponent implements OnInit {
   private getIcon(value: number): L.Icon {
     if (value <= 9) {
       return L.icon({
-        iconUrl: "../../../../assets/icons/1.png",
+        iconUrl: "../../../../assets/icons/virus.png",
         iconSize: [8, 8],
         iconAnchor: [0, 4],
       });
     } else if (value >= 10 && value <= 99) {
       return L.icon({
-        iconUrl: "../../../../assets/icons/2.png",
+        iconUrl: "../../../../assets/icons/virus.png",
         iconSize: [10, 10],
         iconAnchor: [0, 5],
       });
     } else if (value >= 10 && value <= 99) {
       return L.icon({
-        iconUrl: "../../../../assets/icons/3.png",
+        iconUrl: "../../../../assets/icons/virus.png",
         iconSize: [12, 12],
         iconAnchor: [0, 6],
       });
     } else if (value >= 100 && value <= 999) {
       return L.icon({
-        iconUrl: "../../../../assets/icons/4.png",
+        iconUrl: "../../../../assets/icons/virus.png",
         iconSize: [14, 14],
         iconAnchor: [0, 7],
       });
     } else if (value >= 1000 && value <= 9999) {
       return L.icon({
-        iconUrl: "../../../../assets/icons/5.png",
+        iconUrl: "../../../../assets/icons/virus.png",
         iconSize: [16, 16],
         iconAnchor: [0, 8],
       });
     } else if (value >= 10000) {
       return L.icon({
-        iconUrl: "../../../../assets/icons/6.png",
+        iconUrl: "../../../../assets/icons/virus.png",
         iconSize: [18, 18],
         iconAnchor: [0, 9],
       });
