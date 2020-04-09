825c1d09bd365636fd4b647be41bc309eee0bec3
tasos <theodosiout@or-co.gr>
2020-04-09 14:47:44 +0300

    Add google analytics on router evetns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { CovidService } from './shared/services/covid.service';
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+
+declare let gtag: Function;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent{
-  title = 'covid19';
+export class AppComponent {
+  title = "covid19";
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag("config", "UA-140798817-3", {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    });
+  }
 }
