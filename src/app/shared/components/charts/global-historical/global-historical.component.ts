b1691ac1f46c9af9f523bf0b1f91720a0564c6f7
tasos <theodosiout@or-co.gr>
2020-04-12 18:27:56 +0300

    Create global historical data chart

--- src/app/shared/components/charts/global-historical/global-historical.component.ts
+++ src/app/shared/components/charts/global-historical/global-historical.component.ts
@@ -13,82 +13,93 @@ import * as pluginAnnotations from "chartjs-plugin-annotation";
 export class GlobalHistoricalComponent implements OnInit {
   constructor(private covidService: CovidService) {}
   data: Timeline;
+  cases: boolean = true;
+  deaths: boolean = true;
+  recovered: boolean = true;
+
+  casesData: number[] = [];
+  deathsData: number[] = [];
+  recoveredData: number[] = [];
+
   lineChartData: ChartDataSets[];
-  lineChartLabels:Label[];
-  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+  lineChartLabels: Label[];
+  public lineChartOptions: ChartOptions & { annotation: any } = {
     responsive: true,
     scales: {
       // We use this empty structure as a placeholder for dynamic theming.
       xAxes: [{}],
       yAxes: [
         {
-          id: 'y-axis-0',
-          position: 'left',
+          id: "y-axis-0",
+          position: "left",
         },
         {
-          id: 'y-axis-1',
-          position: 'right',
+          id: "y-axis-1",
+          position: "right",
           // gridLines: {
           //   color: rgba(255,0,0,0.3),
           // },
           // ticks: {
           //   fontColor: 'blue',
           // }
-        }
-      ]
+        },
+      ],
     },
     annotation: {
       annotations: [
         {
-          type: 'line',
-          mode: 'vertical',
-          scaleID: 'x-axis-0',
-          value: 'March',
-          borderColor: 'orange',
+          type: "line",
+          mode: "vertical",
+          scaleID: "x-axis-0",
+          value: "March",
+          borderColor: "orange",
           borderWidth: 2,
           label: {
             enabled: true,
-            fontColor: 'orange',
-            content: 'LineAnno'
-          }
+            fontColor: "orange",
+            content: "LineAnno",
+          },
         },
       ],
     },
   };
   public lineChartColors: Color[] = [
-    { // Cases
-      backgroundColor: 'rgba(173, 216, 230, 0.7)',
-      borderColor: 'rgba(173, 216, 230,1)',
-      pointBackgroundColor: 'rgba(173, 216, 230,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(173, 216, 230,0.8)'
+    {
+      // Cases
+      backgroundColor: "rgba(173, 216, 230, 0.7)",
+      borderColor: "rgba(173, 216, 230,1)",
+      pointBackgroundColor: "rgba(173, 216, 230,1)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgba(173, 216, 230,0.8)",
     },
-    { // Deaths
-      backgroundColor: 'rgba(255, 0, 0, 0.7)',
-      borderColor: 'rgba(255, 0, 0,1)',
-      pointBackgroundColor: 'rgba(255, 0, 0,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(255, 0, 0,1)'
+    {
+      // Deaths
+      backgroundColor: "rgba(255, 0, 0, 0.7)",
+      borderColor: "rgba(255, 0, 0,1)",
+      pointBackgroundColor: "rgba(255, 0, 0,1)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgba(255, 0, 0,1)",
     },
-    { // Recovered
-      backgroundColor: 'rgba(32, 178, 170, 0.7)',
-      borderColor: 'rgba(32, 178, 170, 1)',
-      pointBackgroundColor: 'rgba(32, 178, 170,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(32, 178, 170,0.8)'
-    }
+    {
+      // Recovered
+      backgroundColor: "rgba(32, 178, 170, 0.7)",
+      borderColor: "rgba(32, 178, 170, 1)",
+      pointBackgroundColor: "rgba(32, 178, 170,1)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgba(32, 178, 170,0.8)",
+    },
   ];
   public lineChartLegend = true;
-  public lineChartType = 'line';
+  public lineChartType = "line";
   public lineChartPlugins = [pluginAnnotations];
 
   ngOnInit() {
     this.covidService.getGlobalHistoricalData().subscribe(
       (data) => {
-        this.lineChartData=this.convertTimelineToDataset(data);
+        this.lineChartData = this.convertTimelineToDataset(data);
         this.lineChartLabels = this.createChartLabels(data);
         console.log(this.lineChartLabels);
       },
@@ -99,41 +110,37 @@ export class GlobalHistoricalComponent implements OnInit {
   }
 
   private convertTimelineToDataset(timeline: Timeline): ChartDataSets[] {
-    let casesData: number[]=[];
-    let deathsData: number[]=[];
-    let recoveredData: number[]=[];
-
     Object.keys(timeline).forEach((attr1) => {
       console.log(attr1, timeline[attr1]);
       if (attr1 === "cases") {
         Object.keys(timeline[attr1]).forEach((key) => {
-          casesData.push(timeline[attr1][key]);
+          this.casesData.push(timeline[attr1][key]);
         });
       }
       if (attr1 === "deaths") {
         Object.keys(timeline[attr1]).forEach((key) => {
-          deathsData.push(timeline[attr1][key]);
+          this.deathsData.push(timeline[attr1][key]);
         });
       }
       if (attr1 === "recovered") {
         Object.keys(timeline[attr1]).forEach((key) => {
-          recoveredData.push(timeline[attr1][key]);
+          this.recoveredData.push(timeline[attr1][key]);
         });
       }
     });
 
     return [
-      { data: casesData, label: "Cases" },
-      { data: deathsData, label: "Deaths" },
-      { data: recoveredData, label: "Recovered" },
+      { data: this.casesData, label: "Cases" ,order:3},
+      { data: this.deathsData, label: "Deaths" ,order:1},
+      { data: this.recoveredData, label: "Recovered",order:2 },
     ];
   }
-  
-  private createChartLabels(timeline:Timeline):Label[]{
-    const label:Label[]=[];
-    Object.keys(timeline['cases']).forEach(key=>{
+
+  private createChartLabels(timeline: Timeline): Label[] {
+    const label: Label[] = [];
+    Object.keys(timeline["cases"]).forEach((key) => {
       label.push(key);
-    })
+    });
     return label;
   }
 }
